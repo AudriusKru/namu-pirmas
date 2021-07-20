10cb9f96c439e9df26988f001c6d2cecea32538e
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-20 20:19:53 +0300

    master one

---
+++ app/Models/Outfit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Outfit extends Model
+{
+    use HasFactory;
+
+    public function masterOfOutfit()
+    {
+    return $this->belongsTo('App\Models\Master', 'master_id', 'id');
+    }
+}
