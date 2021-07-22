e4a5558fa153382f25bb93e4ba9b766f1902e3ca
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-22 10:11:30 +0300

    sv

--- resources/views/outfit/index.blade.php
+++ resources/views/outfit/index.blade.php
@@ -1,11 +1,32 @@
-@foreach ($outfits as $outfit)
-<a href="{{route('outfit.edit',[$outfit])}}">{{$outfit->type}} size: {{$outfit->size}}</a>
-<span>{{$outfit->masterOfOutfit->name}} {{$outfit->masterOfOutfit->surname}}</span>
-<form method="POST" action="{{route('outfit.destroy', [$outfit])}}">
-@csrf
-<button type="submit">DELETE</button>
-</form>
-<br>@endforeach
-
-
-
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Outfits</div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach ($outfits as $outfit)
+                        <li class="list-group-item">
+                            <div class="list-container">
+                                <div class="list-container__content">
+                                    <span class="ist-container__content__outfit">{{$outfit->type}} size: {{$outfit->size}}</span>
+                                    <span class="ist-container__content__master">{{$outfit->masterOfOutfit->name}} {{$outfit->masterOfOutfit->surname}}</span>
+                                </div>
+                                <div class="list-container__buttons">
+                                    <a href="{{route('outfit.edit',[$outfit])}}" class="btn btn-success">Edit</a>
+                                    <form method="POST" action="{{route('outfit.destroy', [$outfit])}}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </div>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
